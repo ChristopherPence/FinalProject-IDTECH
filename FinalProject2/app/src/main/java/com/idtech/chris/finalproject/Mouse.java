5ab42bbc9a3dbb92a7fb80bea402bc8acd1db99c
ChristopherPence <chris1599@live.com>
2016-07-27 16:53:17 -0400

    FinalProject 1.4.1
    End of the day. No big additions.

--- FinalProject2/app/src/main/java/com/idtech/chris/finalproject/Mouse.java
+++ FinalProject2/app/src/main/java/com/idtech/chris/finalproject/Mouse.java
@@ -1,12 +1,16 @@
 package com.idtech.chris.finalproject;
 
+import android.text.Layout;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by student on 7/27/2016.
  */
 public class Mouse
 {
-    public Mouse(int x, int y)
+    public Mouse(Layout layout, int x, int y)
     {
-        
+
     }
 }
