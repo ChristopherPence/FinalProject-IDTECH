7d148d90f68339e22d50ea1ba32653209f5e92d9
ChristopherPence <chris1599@live.com>
2016-07-28 16:58:13 -0400

    Final Project 1.6
    In the middle of adding login support.

--- FinalProject2/app/src/main/java/com/idtech/chris/finalproject/LoginActivity.java
+++ FinalProject2/app/src/main/java/com/idtech/chris/finalproject/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.idtech.chris.finalproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -20,16 +22,37 @@ public class LoginActivity extends Activity
         getActionBar().hide();
 
         Intent intent = getIntent();
+
+
+
+
     }
 
     public void login(View view)
     {
-
+        EditText user = (EditText) findViewById(R.id.username);
+        EditText pass = (EditText) findViewById(R.id.password);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        if(pass.getText().toString().equals(sharedPref.getString(user.getText().toString(), "")))
+        {
+            System.out.println("Test account logged in");
+        }
+        else
+        {
+            System.out.println("Log in failed");
+        }
     }
 
     public void create(View view)
     {
         EditText user = (EditText) findViewById(R.id.usernameCreate);
+        EditText pass = (EditText) findViewById(R.id.passwordCreate);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+        editor.putString(username, password);
+        editor.commit();
     }
 
 //    @Override
